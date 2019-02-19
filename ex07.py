75510aa110586a2bc8b6bcaec95a21db11630ac7
nikola <nikola.vukcevic2@udg.edu.me>
2019-02-19 23:14:00 +0100

    domaci

---
+++ ex07.py
@@ -0,0 +1,17 @@
+def main():
+
+    print("This program illustrates a chaotic function")
+
+    x = eval(input("Enter a number between 0 and 1: "))
+
+    y = eval(input("Enter another number between 0 and 1: "))
+
+    for i in range(10):
+
+        x = 3.9 * (x - x * x)
+
+        y = 3.9 * (y - y * y)
+
+        print(x, y)
+
+main()
